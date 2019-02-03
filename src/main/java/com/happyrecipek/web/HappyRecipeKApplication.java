47bf320c983886909c6f4634c0a4bdced9b03f11
polal <polal@DESKTOP-VJSIN51>
2019-02-03 21:26:02 +0900

    Created Anything

--- src/main/java/com/happyrecipek/web/HappyRecipeKApplication.java
+++ src/main/java/com/happyrecipek/web/HappyRecipeKApplication.java
@@ -2,20 +2,10 @@ package com.happyrecipek.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HappyRecipeKApplication extends SpringBootServletInitializer{
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder
-					.sources(HappyRecipeKApplication.class);
-	}
-
+public class HappyRecipeKApplication{
 	public static void main(String[] args) {
-		
 		SpringApplication.run(HappyRecipeKApplication.class, args);
 	}
 }
