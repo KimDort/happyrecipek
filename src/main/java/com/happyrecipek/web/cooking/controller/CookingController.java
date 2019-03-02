956577b72f51537b4c9448c7446039612d53c938
youngjunkim <youngjunkim@172.30.1.27>
2019-03-02 17:07:57 +0900

    index & cooking class list page add

--- src/main/java/com/happyrecipek/web/cooking/controller/CookingController.java
+++ src/main/java/com/happyrecipek/web/cooking/controller/CookingController.java
@@ -1,7 +1,11 @@
 package com.happyrecipek.web.cooking.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mobile.device.Device;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,8 +19,26 @@ public class CookingController {
 	private ClassRepository classRepository;
 	
 	@RequestMapping("/cooking/list")
-	public String listPage() {
-		return "web/pc/cooking/list";
+	public String listPage(Device device, Model model) {
+		String returnPage = "";
+
+		/**
+		 * Selection Class List Repository
+		 */
+		List<ClassBase> resultList = classRepository.findAll();
+
+		model.addAttribute("classList", resultList);
+
+		if (device.isMobile()) {
+			returnPage = "web/pc/cooking/list";
+			// returnPage = "web/mobile/admin/class/addClass";
+		} else if (device.isTablet()) {
+			returnPage = "";
+		} else if (device.isNormal()) {
+			returnPage = "web/pc/cooking/list";
+		}
+
+		return returnPage;
 	}
 	
 	/**
