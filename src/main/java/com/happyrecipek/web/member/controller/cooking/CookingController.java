b2187969c31c6c808ba8bca94fe4b459cbe6975d
polales <polal@61.73.71.245>
2019-05-23 23:48:25 +0900

    add class join controller page

--- src/main/java/com/happyrecipek/web/member/controller/cooking/CookingController.java
+++ src/main/java/com/happyrecipek/web/member/controller/cooking/CookingController.java
@@ -2,11 +2,14 @@ package com.happyrecipek.web.member.controller.cooking;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mobile.device.Device;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.happyrecipek.web.admin.cookingclass.entities.ClassBase;
@@ -14,7 +17,9 @@ import com.happyrecipek.web.admin.cookingclass.repositories.ClassRepository;
 
 @Controller
 public class CookingController {
-
+	
+	private static final Logger log = LogManager.getLogger(CookingController.class);
+	
 	@Autowired
 	private ClassRepository classRepository;
 
@@ -51,5 +56,32 @@ public class CookingController {
 		ClassBase getDetail = new ClassBase();
 		getDetail = classRepository.findByClassSeq(classSeq);
 		return getDetail;
+	}
+	
+	/**
+	 * @author KYJ
+	 * @since 2019-05-25
+	 * @param Device
+	 * @return String Cooking Class Detail Page
+	 */
+	@RequestMapping("/cooking/joinPage")
+	public String classJoinPage(Device device, Model model, @RequestParam("classNum")int classNum) {
+		String returnPage = "";
+		/**
+		 * Selection Class List Repository
+		 */
+		ClassBase result = classRepository.findByClassSeq(classNum);
+
+		model.addAttribute("classInfo", result);
+
+		if (device.isMobile()) {
+			returnPage = "web/mobile/cooking/join";
+		} else if (device.isTablet()) {
+			returnPage = "web/mobile/cooking/join";
+		} else if (device.isNormal()) {
+			returnPage = "web/pc/cooking/join";
+		}
+
+		return returnPage;
 	}
 }
