21878309283a61ea3815a9a0848c04c6fa8c7256
polal <polal@DESKTOP-49MKE9R>
2019-12-25 23:10:27 +0900

    ckeditor youtubu plugin add

--- src/main/resources/static/assets/ckeditor/config.js
+++ src/main/resources/static/assets/ckeditor/config.js
@@ -7,4 +7,10 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.extraPlugins = 'youtube';
+	config.youtube_width = '100%';
+	config.youtube_height = '100%';
+	config.youtube_responsive = true;
+	config.youtube_older = false;
+	config.youtube_controls = true;
 };
